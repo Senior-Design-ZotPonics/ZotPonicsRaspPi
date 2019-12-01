beb0444b67bf79615cb927c824d875819f85c9b1
okyang <okyang@uci.edu>
2019-11-30 21:37:32 -0800

    adjusted servo settings

--- sensors_tests/servoTest.py
+++ sensors_tests/servoTest.py
@@ -5,7 +5,7 @@
 import RPi.GPIO as GPIO
 import time
 
-control = [5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
+control = [3,12]
 
 servo = 25 #GPIO pin
 
@@ -22,6 +22,12 @@ GPIO.setup(servo,GPIO.OUT)
 # duty cycle for 90 degree = (1.5/20)*100 = 7.5%
 # duty cycle for 180 degree = (2/20)*100 = 10%
 
+#<oky>: however, servo motors are not perfect
+# after testing:
+# duty cycle for 0 degree = 3%
+# duty cycle for 180 degree = 12%
+
+
 p=GPIO.PWM(servo,50)# 50hz frequency
 
 p.start(2.5)# starting duty cycle ( it set the servo to 0 degree )
@@ -29,16 +35,10 @@ p.start(2.5)# starting duty cycle ( it set the servo to 0 degree )
 
 try:
        while True:
-           for x in range(11):
+           for x in range(2):
              p.ChangeDutyCycle(control[x])
-             print(x)
+             print(control[x])
              time.sleep(2)
-
-
-           # for x in range(9,0,-1):
-           #   p.ChangeDutyCycle(control[x])
-           #   time.sleep(0.03)
-           #   print x
 
 except KeyboardInterrupt:
     GPIO.cleanup()
