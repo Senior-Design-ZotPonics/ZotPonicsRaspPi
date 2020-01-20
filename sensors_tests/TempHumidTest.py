e774e9170f93503cb86d185c37b92e008db79b8a
okyang <owenyang123@gmail.com>
2020-01-19 23:14:03 -0800

    fixed type in code

--- sensors_tests/TempHumidTest.py
+++ sensors_tests/TempHumidTest.py
@@ -1,8 +1,12 @@
 #Resources:
 #- https://tutorials-raspberrypi.com/raspberry-pi-measure-humidity-temperature-dht11-dht22/
 import Adafruit_DHT
+import time
 
 if __name__ == "__main__":
     sensor = Adafruit_DHT.DHT11
     pin = 4
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        print("Humidity(%):",humidity,"|", "Temperature(C):", temperature)
+        time.sleep(2)
